6ed19eaa8521bae116678a3c8a3f99c9ee97d1f6
YelloFattyBean <jesse.geiss@gmail.com>
2020-10-07 16:55:01 -0700

    update index.js to get request

--- server/index.js
+++ server/index.js
@@ -8,27 +8,45 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/api/products/:id', (req, res) => {
-  res.redirect(`http://localhost:3002${req.url}`)
+  // res.redirect(`http://localhost:3002${req.url}`)
+  axios.get(`http://localhost:3002${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => res.send(err))
 });
 
 app.get('/api/products/questions/sort/:sort', (req, res) => {
-  res.redirect(`http://localhost:3001${req.url}`)
+  // res.redirect(`http://localhost:3001${req.url}`)
+  axios.get(`http://localhost:3001${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => res.send(err))
 });
 
 app.get('/api/products/questions/:question_id', (req, res) => {
-  res.redirect(`http://localhost:3001${req.url}`)
+  // res.redirect(`http://localhost:3001${req.url}`)
+  axios.get(`http://localhost:3001${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => res.send(err))
 });
 
 app.get('/api/products/:id/relatedItems', (req, res) => {
-  res.redirect(`http://localhost:3004${req.url}`)
+  // res.redirect(`http://localhost:3004${req.url}`)
+  axios.get(`http://localhost:3004${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => res.send(err))
 });
 
 app.get('/api/products/reviews/avg/:item', (req, res) => {
-  res.redirect(`http://localhost:3005${req.url}`)
+  // res.redirect(`http://localhost:3005${req.url}`)
+  axios.get(`http://localhost:3005${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => res.send(err))
 });
 
 app.get('/api/products/reviews/:id/:count/:sort', (req, res) => {
-  res.redirect(`http://localhost:3005${req.url}`)
+  // res.redirect(`http://localhost:3005${req.url}`)
+  axios.get(`http://localhost:3005${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => res.send(err))
 });
 
 
