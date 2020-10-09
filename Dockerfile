2274b40437e3125db51e37ef00073989303cf85b
YelloFattyBean <jesse.geiss@gmail.com>
2020-10-09 11:08:02 -0700

    update proxy to local cache

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.18-alpine
+FROM node:10.22
 ENV NODE_ENV production
 WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
--- server/index.js
+++ server/index.js
@@ -10,42 +10,42 @@ app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/api/products/:id', (req, res) => {
   // res.redirect(`http://localhost:3002${req.url}`)
-  axios.get(`http://localhost:3002${req.url}`)
+  axios.get(`http://ec2-34-213-37-84.us-west-2.compute.amazonaws.com:80${req.url}`)
   .then(({ data }) => res.send(data))
   .catch((err) => res.send(err))
 });
 
 app.get('/api/products/questions/sort/:sort', (req, res) => {
   // res.redirect(`http://localhost:3001${req.url}`)
-  axios.get(`http://localhost:3001${req.url}`)
+  axios.get(`http://ec2-54-186-31-137.us-west-2.compute.amazonaws.com:80${req.url}`)
   .then(({ data }) => res.send(data))
   .catch((err) => res.send(err))
 });
 
 app.get('/api/products/questions/:question_id', (req, res) => {
   // res.redirect(`http://localhost:3001${req.url}`)
-  axios.get(`http://localhost:3001${req.url}`)
+  axios.get(`http://ec2-54-186-31-137.us-west-2.compute.amazonaws.com:80${req.url}`)
   .then(({ data }) => res.send(data))
   .catch((err) => res.send(err))
 });
 
 app.get('/api/products/:id/relatedItems', (req, res) => {
   // res.redirect(`http://localhost:3004${req.url}`)
-  axios.get(`http://localhost:3004${req.url}`)
+  axios.get(`http://ec2-52-24-22-173.us-west-2.compute.amazonaws.com:80${req.url}`)
   .then(({ data }) => res.send(data))
   .catch((err) => res.send(err))
 });
 
 app.get('/api/products/reviews/avg/:item', (req, res) => {
   // res.redirect(`http://localhost:3005${req.url}`)
-  axios.get(`http://localhost:3005${req.url}`)
+  axios.get(`http://ec2-52-33-159-141.us-west-2.compute.amazonaws.com:80${req.url}`)
   .then(({ data }) => res.send(data))
   .catch((err) => res.send(err))
 });
 
 app.get('/api/products/reviews/:id/:count/:sort', (req, res) => {
   // res.redirect(`http://localhost:3005${req.url}`)
-  axios.get(`http://localhost:3005${req.url}`)
+  axios.get(`http://ec2-52-33-159-141.us-west-2.compute.amazonaws.com:80${req.url}`)
   .then(({ data }) => res.send(data))
   .catch((err) => res.send(err))
 });
